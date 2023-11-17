826a01aca676edc25a275dffca1b022bff752dfb
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-11-17 10:21:46 +0700

    set up express with ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to API v1");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
